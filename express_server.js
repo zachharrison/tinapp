47aa0d86d2029aa7011fe4cdf40365e7b4b7e164
Zach Harrison <zharrison100@gmail.com>
2020-12-08 01:28:18 +0000

    Added random string generator

--- express_server.js
+++ express_server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
+
+// USE BODY PARSER TO MAKE POST REQUESTS
+app.use(bodyParser.urlencoded({extended: true}));
 
 // SET EJS AS THE VIEW ENGINE
 app.set('view engine', 'ejs');
+
+const generateRandomString = () => {
+  let result = [];
+  while(result.length < 6) {
+    result.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+  }
+  result.forEach((x, i, a) => {
+    if (i % 2 === 0) {
+      a[i] = a[i].toUpperCase();
+    }
+  });
+  return result.join('');s
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +31,8 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+/*~~~~~~~~~~~~~~~~URL ROUTE HANDLERS~~~~~~~~~~~~~~~~*/
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
@@ -23,7 +42,16 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });

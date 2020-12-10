6c92204ba1fc08286bfe591de40f17f797ffbbc0
Zach Harrison <zharrison100@gmail.com>
2020-12-10 17:27:23 +0000

    Implemented basic permission features

--- express_server.js
+++ express_server.js
@@ -21,19 +21,26 @@ const users = {
   },
   "user2": {
     id: "user2", 
-    email: "user@example.com2", 
+    email: "user2@example.com", 
     password: "purple"
   }
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { 
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID" 
+  },
+  "9sm5xK": { 
+    longURL: "http://www.google.com",
+    userID: "user2"
+  }
 };
 
 /*********************************************************************
                           HELPER FUNCTIONS
 ***********************************************************************/
+
 const generateRandomString = () => {
   let result = [];
   let i = 0;
@@ -55,38 +62,86 @@ const findWithEmail = (email) => {
   return false;
 };
 
+const findUrlsForUser = (id) => {
+  let result = {};
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      result[url] = urlDatabase[url];
+    }
+  }
+  return result;
+};
+
 /*********************************************************************
                           URL ROUTE HANDLERS
 ***********************************************************************/
+
 app.get('/urls/register', (req, res) => {
-  const templateVars = { user_id: users[req.cookies['user_id']] };
+
+  const templateVars = { 
+    user_id: users[req.cookies['user_id']],
+  };
+
   res.render('urls_register', templateVars);
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, user_id: users[req.cookies['user_id']]};
+  
+  if (!req.cookies['user_id']) {
+    return res.redirect('/urls/login');
+  }
+
+  const userURLS = findUrlsForUser(req.cookies['user_id']);
+  
+  const templateVars = { 
+    urls: userURLS, 
+    user_id: users[req.cookies['user_id']],
+  };
+
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/login', (req, res) => {
-  const templateVars = { user_id: users[req.cookies['user_id']] };
+
+  const templateVars = { 
+    user_id: users[req.cookies['user_id']], 
+  };
+
   res.render('urls_login', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  const templateVars = { user_id: users[req.cookies['user_id']] }
-  res.render('urls_new', templateVars)
+  
+  if (!req.cookies['user_id']) {
+    return res.redirect('/urls/login');
+  }
+  
+  const templateVars = { 
+    user_id: users[req.cookies['user_id']] 
+  };
+
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user_id: users[req.cookies['user_id']] };
-  urlDatabase.hasOwnProperty(req.params.shortURL) ? res.render('urls_show', templateVars) : 
+
+  if (!req.cookies['user_id']) {
+    return res.redirect('/urls/login');
+  }
+
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]['longURL'], 
+    user_id: users[req.cookies['user_id']],
+  };
+
+  urlDatabase.hasOwnProperty(req.params.shortURL) ? 
+  res.render('urls_show', templateVars) : 
   res.render('urls_error', templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
-  urlDatabase.hasOwnProperty(req.params.shortURL) ? res.redirect(urlDatabase[req.params.shortURL]) : 
-  res.render('urls_error');
+  res.redirect(urlDatabase[req.params.shortURL]['longURL']);
 });
 
 app.post('/register', (req, res) => {
@@ -144,11 +199,26 @@ app.post('/urls/:shortURL', (req, res) => {
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  const longURL = req.body.longURL;
+  const userID = req.cookies['user_id'];
+
+  urlDatabase[shortURL] = {
+    longURL, 
+    userID
+  };
+
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
+  const userID = req.cookies['user_id'];
+  const shortURL = req.params['shortURL'];
+  // const urlObj = findUrlsForUser(userID);
+  // console.log(shortURL);
+  if (!findUrlsForUser(userID)[shortURL]) {
+    return res.redirect("/urls/login");
+  }
+
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });

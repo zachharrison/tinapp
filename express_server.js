f4f9cae249fa478f4a8df6cc8a3e1ce495935720
Zach Harrison <zharrison100@gmail.com>
2020-12-10 06:08:33 +0000

    Refactored helper functions and fixed all bugs

--- express_server.js
+++ express_server.js
@@ -13,6 +13,27 @@ app.set('view engine', 'ejs');
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2": {
+    id: "user2", 
+    email: "user@example.com2", 
+    password: "purple"
+  }
+};
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+/*********************************************************************
+                          HELPER FUNCTIONS
+***********************************************************************/
 const generateRandomString = () => {
   let result = [];
   let i = 0;
@@ -25,24 +46,40 @@ const generateRandomString = () => {
   return result.join('');
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const findWithEmail = (email) => {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return false;
 };
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~URL ROUTE HANDLERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*********************************************************************
+                          URL ROUTE HANDLERS
+***********************************************************************/
+app.get('/urls/register', (req, res) => {
+  const templateVars = { user_id: users[req.cookies['user_id']] };
+  res.render('urls_register', templateVars);
+});
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies['username']};
+  const templateVars = { urls: urlDatabase, user_id: users[req.cookies['user_id']]};
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/login', (req, res) => {
+  const templateVars = { user_id: users[req.cookies['user_id']] };
+  res.render('urls_login', templateVars);
+});
+
 app.get('/urls/new', (req, res) => {
-  const templateVars = { username:req.cookies['username'] }
+  const templateVars = { user_id: users[req.cookies['user_id']] }
   res.render('urls_new', templateVars)
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username'] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user_id: users[req.cookies['user_id']] };
   urlDatabase.hasOwnProperty(req.params.shortURL) ? res.render('urls_show', templateVars) : 
   res.render('urls_error', templateVars);
 });
@@ -52,13 +89,50 @@ app.get('/u/:shortURL', (req, res) => {
   res.render('urls_error');
 });
 
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const id = generateRandomString();
+
+  const user = {
+    id, 
+    email,
+    password
+  }
+
+  if (!email || !password) {
+    return res.status(400).send('Please fill in all fields');
+  }
+  
+  if (findWithEmail(email)) {
+    return res.status(400).send('Email is already registered');
+  }
+  
+  users[id] = user;
+
+  res.cookie('user_id', id);
+
+  res.redirect('/urls');
+});
+
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username)
-  res.redirect('/urls')
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = findWithEmail(email);
+  const id = user.id;
+
+  if (user.email !== email) {
+    return res.status(403).send('Email or password is incorrect, try again.');
+  } else if (user.password !== password) {
+    return res.status(403).send('Email or password is incorrect, try again.');
+  }
+
+  res.cookie('user_id', id);
+  res.redirect('/urls');
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
